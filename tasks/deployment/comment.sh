3adf268edaf1a894d3c5b2bc7ea5d971c3dc2bed
Luis Tan <33774265+LuisTan@users.noreply.github.com>
2020-05-20 11:15:42 +0800

    Reconfigure CCI config and commands

--- tasks/deployment/comment.sh
+++ tasks/deployment/comment.sh
@@ -2,9 +2,14 @@
 
 set -eu
 
+  
+github_user=${GITHUB_PR_COMMENTER:?"Missing GITHUB_PR_COMMENTER environment variable"}
+
+github_token=${GITHUB_PR_TOKEN:?"Missing GITHUB_PR_TOKEN environment variable"}
+
 if ! repository=$(curl -s \
                   -X GET \
-                  -H "Authorization: bearer ${token}" \
+                  -H "Authorization: bearer ${GITHUB_PR_TOKEN}" \
                   -d "{}" \
                   -H "Content-Type: application/json" \
                   "https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"); then
@@ -18,10 +23,6 @@ if ! repository_id=$(echo "${repository}" | python -c 'import sys, json; print j
 fi
 
 artifacts_url="https://${CIRCLE_BUILD_NUM}-${repository_id}-gh.circle-artifacts.com/0"
-  
-github_user=${GITHUB_PR_COMMENTER:?"Missing GITHUB_PR_COMMENTER environment variable"}
-
-github_token=${GITHUB_PR_TOKEN:?"Missing GITHUB_PR_TOKEN environment variable"}
 
 sudo apt-get install jq
 
@@ -32,11 +33,13 @@ if [ $(echo $pr_response | jq length) -eq 0 ]; then
   echo "No PR found to update"
 else
   pr_comment_url=$(echo $pr_response | jq -r ".[]._links.comments.href")
+  
+  echo ${pr_comment_url}
 
-  curl --location --request POST "$pr_comment_url" \
-  -u $github_user:$github_token \
-  --header 'Content-Type: application/json' \
-  --data-raw '{
-  "body": "Link for the Storybook (Active only for 30 days) '${artifacts_url}'/storybook-static/index.html \n Link for the Coverage (Active only for 30 days) '${artifacts_url}'/coverage/lcov-report/src/index.html"
-  }'
+    curl --location --request POST "$pr_comment_url" \
+    -u $github_user:$github_token \
+    --header 'Content-Type: application/json' \
+    --data-raw '{
+    "body": "Link for the Storybook (Active only for 30 days) '${artifacts_url}'/storybook-static/index.html \n Link for the Coverage (Active only for 30 days) '${artifacts_url}'/coverage/lcov-report/src/index.html"
+    }'
 fi
